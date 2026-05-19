43e094d8f5a78295684780fc7224328d1d99c567
albar <tomascuestalopez2305@gmail.com>
2026-05-18 20:37:49 -0500

    correcciones persistencias en empleados

---
+++ IPersistencia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaPapas
+{
+    public interface IPersistencia
+    {
+        void GuardarTodos();
+    }
+
+        public class PersistenciaService : IPersistencia
+        {
+            public void GuardarTodos()
+            {
+                Console.WriteLine("Guardando datos...");
+
+                // Aquí llaman sus métodos existentes
+                Program.GuardarProductos();
+                Program.GuardarSedes();
+                Program.GuardarCarta();
+            }
+        }
+}
+
---
+++ Sede.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaPapas
+{
+    public class Sede
+    {
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+        public List<Mesero> Meseros { get; set; } = new();
+        public List<Cocinero> Cocineros { get; set; } = new();
+        public List<Empleado> Empleados { get; set; } = new();
+
+        public event EventHandler<SedeAgregadaEventArgs> SedeAgregada;
+
+
+        public Sede(string nombre, string ubicacion )
+        {
+            Nombre = nombre;
+            Ubicacion = ubicacion;
+           
+        }
+        public void AgregarSede()
+        {
+            Console.WriteLine("Sede agregada correctamente");
+
+            OnSedeAgregada();
+        }
+
+        protected virtual void OnSedeAgregada()
+        {
+            SedeAgregada?.Invoke(
+                this, new SedeAgregadaEventArgs(Nombre, Ubicacion)
+            );
+        }
+        public void AgregarEmpleado(Empleado emp)
+        {
+            if (emp is Mesero m)
+            {
+                Meseros.Add(m);
+                Empleados.Add(m);
+
+            }
+            else if (emp is Cocinero c)
+            {
+                Cocineros.Add(c);
+                Empleados.Add(c);
+
+            }
+        }
+        public void EliminarEmpleado(Empleado empleado)
+        {
+    
+                if (empleado == null)
+                {
+                    throw new Exception("estas eliminando un empleado que no existe");
+                }
+                else
+                {
+                    if(empleado is Mesero m)
+                    {
+                            Meseros.Remove(empleado as Mesero);
+                    }
+                    else
+                    {
+                        Cocineros.Remove(empleado as Cocinero);
+                    }
+                    Empleados.Remove(empleado);
+                }
+        }
+    }
+}
